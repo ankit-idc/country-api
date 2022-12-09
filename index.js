95fcedbf523397ec7d15cf2c8abc878a5dcd5ffa
Ankit-idc <ankit.bhowmik@idreamcareer.com>
2022-12-09 10:48:45 +0530

    added try catch

--- index.js
+++ index.js
@@ -9,75 +9,87 @@ const app = express();
 app.use(cors("*"));
 
 app.get("/country", (req, res) => {
-  const countryList = [];
-  for (let country of countries) {
-    countryList.push({
-      id: country.id,
-      name: country.name,
-      emoji: country.emoji,
-    });
+  try {
+    const countryList = [];
+    for (let country of countries) {
+      countryList.push({
+        id: country.id,
+        name: country.name,
+        emoji: country.emoji,
+      });
+    }
+    res.send(countryList);
+  } catch (err) {
+    res.status(500).send([]);
   }
-  res.send(countryList);
 });
 
 app.get("/state/:country_name", (req, res) => {
-  const countryName = req.params.country_name;
-  let selectedCountry = {};
+  try {
+    const countryName = req.params.country_name;
+    let selectedCountry = {};
 
-  //not using arr.find method for optimisation.
-  for (let country of countries) {
-    if (country.name === countryName) {
-      selectedCountry = country;
-      break;
+    //not using arr.find method for optimisation.
+    for (let country of countries) {
+      if (country.name === countryName) {
+        selectedCountry = country;
+        break;
+      }
     }
-  }
 
-  if (!selectedCountry.name) {
-    return res.send([]);
-  }
+    if (!selectedCountry.name) {
+      return res.send([]);
+    }
 
-  return res.send(
-    selectedCountry?.states?.map((state) => ({
-      name: state.name,
-      id: state.id,
-      state_code: state.state_code,
-    }))
-  );
+    return res.send(
+      selectedCountry?.states?.map((state) => ({
+        name: state.name,
+        id: state.id,
+        state_code: state.state_code,
+      }))
+    );
+  } catch (err) {
+    res.status(500).send([]);
+  }
 });
 
 app.get("/city/:state_name/:country_name", (req, res) => {
-  const countryName = req.params.country_name;
-  const stateName = req.params.state_name;
-  let selectedCountry = {};
-  let selectedState = {};
+  try {
+    const countryName = req.params.country_name;
+    const stateName = req.params.state_name;
+    let selectedCountry = {};
+    let selectedState = {};
 
-  //not using arr.find method for optimisation.
-  for (let country of countries) {
-    if (country.name === countryName) {
-      selectedCountry = country;
-      break;
+    //not using arr.find method for optimisation.
+    for (let country of countries) {
+      if (country.name === countryName) {
+        selectedCountry = country;
+        break;
+      }
     }
-  }
 
-  if (!selectedCountry.name) {
-    return res.send([]);
-  }
-
-  //not using arr.find method for optimisation.
-  for (let state of selectedCountry.states) {
-    if (state.name === stateName) {
-      selectedState = state;
-      break;
+    if (!selectedCountry.name) {
+      return res.send([]);
     }
-  }
 
-  if (!selectedState.name) {
-    return res.send([]);
-  }
+    //not using arr.find method for optimisation.
+    for (let state of selectedCountry.states) {
+      if (state.name === stateName) {
+        selectedState = state;
+        break;
+      }
+    }
 
-  res.send(
-    selectedState.cities.map((city) => ({ id: city.id, name: city.name }))
-  );
+    if (!selectedState.name) {
+      return res.send([]);
+    }
+
+    res.send(
+      selectedState.cities.map((city) => ({ id: city.id, name: city.name }))
+    );
+  } catch (err) {
+    res.status(500).send([]);
+  }
 });
 
 const PORT = process.env.PORT;
